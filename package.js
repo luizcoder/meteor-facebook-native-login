e386f5db58945d0f8c429e3fc32f4347baafa340
Luiz Martins <luiz@luizcoder.com.br>
2017-08-18 15:36:42 -0300

    bump version 0.1.1

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'jsep:facebook-native-login',
-  version: '0.1.0',
+  name: 'luizcoder:facebook-native-login',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Use native api to login with Facebook',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jsep/meteor-facebook-native-login.git',
+  git: 'https://github.com/luizcoder/meteor-facebook-native-login.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
